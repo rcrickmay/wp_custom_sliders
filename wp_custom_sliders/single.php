a8d3b117688a3f7253b45c3bc2f509472e8f9831
rcrickmay <rcrickmay@gmail.com>
2016-08-12 11:16:07 -0700

    Reduced variable dependencies
    Please refer to Readme for full notes on changes

--- wp_custom_sliders/single.php
+++ wp_custom_sliders/single.php
@@ -3,35 +3,17 @@
 
 <!-- GET SILDER -->
 <?php
-    //Get post variables
-    //store post_slug in session to get slides
-    global $post; 
-    $page_slug = $post -> post_name;
-    $_SESSION['se_page_slug'] = strtolower($page_slug);
-    
     //Get meta data to check if page has slides
-    //page will load unique slides if '$key_1_name' is
-    //'use_slides' and '$key_1_value' = 'unique'
+    //page will load unique slides if '$key_name' is
+    //'use_slides' and '$key_value' = ['sliders_folder_name']
     //otherwise default (blank) slides load
-    $key_1_value = get_post_meta( get_the_ID(), 'custom_sliders', true );
-    $confirm_custom = 'custom';
-?>
+    $key_value = get_post_meta( get_the_ID(), 'custom_sliders', true );
+    $_SESSION['se_custom_sliders'] = strtolower($key_value);
 
-<?php
-//CUSTOM SLIDER CONDITIONS
-    if ( ! empty( $key_1_value ) ) {
-        
-      if ( $key_1_value == $confirm_custom ) {
-          $_SESSION['se_custom_sliders'] = strtolower($key_1_value);
-          get_template_part( 'sliders/sliders', $_SESSION['se_custom_sliders'] );
-        } else {
-          get_template_part( 'sliders/sliders-generic' );
-      }
-      
-    } else {
-        get_template_part( 'sliders/sliders' );
-    }   
-    
+    if ( ! empty( $key_value ) ) {
+      get_template_part( 'sliders/sliders', 'custom' );
+    }
+
 ?>
 
 
